0ffa04aea5e8a351a40995720dc142d7bf1258ef
rayhanbeg <mdrayhan40301@gmail.com>
2024-02-21 16:42:19 +0600

    finish projects

--- script.js/bus.js
+++ script.js/bus.js
@@ -1,22 +1,3 @@
-// function buttonId(){
-//     hideElementId('header');
-//     hideElementId('offer-section');
-// }
-
-
-
-
-
-
-// function selectBgButton(button){
-//     var buttons = document.querySelectorAll('.btn');
-//     buttons.forEach(function(btn){
-//         btn.classList.add('selected')
-//     });
-//     button.classList.remove('selected')
-// }
-
-// Ticket select-------
 
 let totalCost = 0;
 let grandPrice = 0;
@@ -50,10 +31,11 @@ for(const btn of allBtn){
         const buttonText = button.innerText;
     
         totalCost += 550;
-        console.log('Total cost', totalCost);
+        const totalCostConvert = parseInt(totalCost);
+        console.log('Total cost', totalCostConvert);
     
         // update cost---
-        document.getElementById('total-price').innerText = totalCost;
+        document.getElementById('total-price').innerText = totalCostConvert;
 
 
 
@@ -80,6 +62,7 @@ for(const btn of allBtn){
         totalCost -= 550;
         return;
     }
+
 
 
         // const placeName = e.target.parentNode.childNodes[1].innerText;
@@ -111,9 +94,9 @@ for(const btn of allBtn){
 
 
 
-
-
-
+        
+        updateGrandTotal();
+        checkInputs();
 
     });
 }
@@ -122,28 +105,35 @@ for(const btn of allBtn){
 
         // coupon section------
 
- 
-        const grand = event.target;
-        const grandText = grand.innerText;
 
-        grandPrice += 550;
-        console.log('grandCost', grandPrice);
-    
-        // update cost---
-        document.getElementById('total-price-discount').innerText = grandPrice;
 
-        function couponButton(){
-            const couponCode = document.getElementById('coupon-code').value;
-            if(couponCode === 'NEW15'){
-                totalCost *= 0.85;
-                console.log(couponCode);
 
-            }
-            else if( couponCode === 'COUPLE20'){
-                totalCost *= 0.8;
+        function updateGrandTotal(control) {
+            const previousTotal = document.getElementById("total-price").innerText;
+            const convertedTotal = parseInt(previousTotal);
+            document.getElementById("total-price-discount").innerText = convertedTotal;
+                   console.log('grand-total', convertedTotal);
+            const couponCode = document.getElementById("coupon-code").value;
+            let discount = 0;
+            if (control) {
+              if (couponCode == "NEW15") {
+                discount = convertedTotal * 0.15;
+              
+              } else if (couponCode === "COUPLE20") {
+                discount = convertedTotal * 0.20;
                 
+             } else {
+                alert("Invalid Coupon Code No Discount");
+                return;
+              }
+
+              document.getElementById("total-price-discount").innerText =
+              convertedTotal - discount;
+
+            } else {
+              document.getElementById("total-price-discount").innerText = convertedTotal;
             }
-        }
+          }
 
 
 
@@ -151,6 +141,31 @@ for(const btn of allBtn){
 
 
 
+ // Get the input elements
+ const nameInput = document.getElementById('nameInput');
+ const numberInput = document.getElementById('numberInput')
+ const emailInput = document.getElementById('emailInput');
+ const nextButton = document.getElementById('nextButton');
+
+ // Function to check if the input fields are filled
+ function checkInputs() {
+   const nameValue = nameInput.value.trim();
+   const emailValue = emailInput.value.trim();
+   const numberValue = numberInput.value.trim();
+
+   // Enable the Next button only if name and email are not empty
+   if (nameValue !== '' && emailValue !== '' && numberValue !== '') {
+     nextButton.disabled = false;
+   }
+    else {
+     nextButton.disabled = true;
+   }
+ }
+
+ // Call the function whenever there's a change in the input fields
+ nameInput.addEventListener('input', checkInputs);
+ numberInput.addEventListener('input', checkInputs);    
+ emailInput.addEventListener('input', checkInputs);    
 
 
-
+ 
--- script.js/utility.js
+++ script.js/utility.js
@@ -16,3 +16,4 @@ function setInnerText(id, value) {
 
 
 
+
